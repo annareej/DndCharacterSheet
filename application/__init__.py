6a94bb1890de36e00848d7bfb8a2b2c8946acfc5
Anna-Reetta Julkunen <ar.julkunen@gmail.com>
2020-04-01 20:53:51 +0300

    Added support for PostgreSQL in Heroku

--- application/__init__.py
+++ application/__init__.py
@@ -2,8 +2,14 @@ from flask import Flask
 app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///charactersheets.db"
-app.config["SQLALCHEMY_ECHO"] = True
+
+import os
+
+if os.environ.get("HEROKU"):
+	app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+	app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///charactersheets.db"
+	app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
@@ -32,4 +38,7 @@ login_manager.login_message = "Please login ot use this functionality."
 def load_user(user_id):
 	return User.query.get(user_id)
 
-db.create_all()
+try:
+	db.create_all()
+except:
+	pass
